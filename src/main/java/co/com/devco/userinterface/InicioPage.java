26a39b7b393a19cd8ad8d0745765d265c0688de4
jfernandezt1 <jfernandez@todo1.net>
2020-11-23 16:12:20 -0500

    Se crea base del proyecto para e-commerce

--- src/main/java/co/com/devco/userinterface/InicioPage.java
+++ src/main/java/co/com/devco/userinterface/InicioPage.java
@@ -2,10 +2,8 @@ package co.com.devco.userinterface;
 
 import io.appium.java_client.MobileBy;
 import net.serenitybdd.screenplay.targets.Target;
-import org.openqa.selenium.By;
 
 public class InicioPage {
     public static final Target BTN_LUPA = Target.the("Boton para busqueda").located(MobileBy.AccessibilityId("Search…"));
     public static final Target TXT_BUSQUEDA_PRODUCTOS = Target.the("Campo de texto para busqueda").located(MobileBy.id("com.solodroid.solomerce:id/search_src_text"));
-    public static final Target IMG_PRODUCTO = Target.the("Contenedor del producto a comprar").locatedBy("//android.widget.TextView[contains(@text,'Nike')]");
 }
--- src/test/java/co/com/devco/stepdefinitions/ComprarProductosStepDefinition.java
+++ src/test/java/co/com/devco/stepdefinitions/ComprarProductosStepDefinition.java
@@ -1,8 +1,6 @@
 package co.com.devco.stepdefinitions;
 
-import co.com.devco.tasks.AgregarProducto;
 import co.com.devco.tasks.BuscarProducto;
-import co.com.devco.tasks.RealizarCompra;
 import cucumber.api.java.Before;
 import cucumber.api.java.es.Cuando;
 import cucumber.api.java.es.Dado;
@@ -10,11 +8,10 @@ import cucumber.api.java.es.Entonces;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
-public class BuyingProductsStepDefinition {
+public class ComprarProductosStepDefinition {
 
     @Before
     public void prepareStage() {
@@ -24,15 +21,13 @@ public class BuyingProductsStepDefinition {
     @Dado("que (.*) agrega los tennis (.*) al carrito")
     public void agregarTennisAlCarrito(String actor, String producto) {
         theActorCalled(actor).attemptsTo(
-                BuscarProducto.enEcommerce(producto),
-                AgregarProducto.alCarrito(producto)
+                BuscarProducto.enEcommerce(producto)
         );
     }
 
     @Cuando("realiza el chackout de su compra")
     public void actorPutShirtCart() {
         theActorInTheSpotlight().attemptsTo(
-                RealizarCompra.deProductos()
         );
     }
 
