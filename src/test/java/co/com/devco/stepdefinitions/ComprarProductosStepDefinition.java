a1841aadecddb48b5f62a9e4c01eb5281defaa69
jfernandezt1 <jfernandez@todo1.net>
2021-02-09 08:18:25 -0500

    Se crea escenario base para Swaglabs

--- src/test/java/co/com/devco/stepdefinitions/ComprarProductosStepDefinition.java
+++ src/test/java/co/com/devco/stepdefinitions/ComprarProductosStepDefinition.java
@@ -1,21 +1,15 @@
 package co.com.devco.stepdefinitions;
 
-import co.com.devco.exceptions.PurchaseIsNotCompleteException;
-import co.com.devco.questions.Purchase;
-import co.com.devco.tasks.AddProduct;
 import co.com.devco.tasks.Login;
 import cucumber.api.java.Before;
-import cucumber.api.java.en.And;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java.es.Cuando;
+import cucumber.api.java.es.Dado;
+import cucumber.api.java.es.Entonces;
+import cucumber.api.java.es.Y;
 import net.serenitybdd.screenplay.actors.OnStage;
 import net.serenitybdd.screenplay.actors.OnlineCast;
 
-import static co.com.devco.exceptions.PurchaseIsNotCompleteException.PURCHASE_FAILED_MESSAGE_EXCEPTION;
-import static co.com.devco.tasks.Checkout.checkout;
-import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
-import static net.serenitybdd.screenplay.actors.OnStage.*;
+import static net.serenitybdd.screenplay.actors.OnStage.theActorCalled;
 
 public class ComprarProductosStepDefinition {
 
@@ -24,27 +18,22 @@ public class ComprarProductosStepDefinition {
         OnStage.setTheStage(new OnlineCast());
     }
 
-    @Given("que (.*) se cuentra en Swaglabs App")
+    @Dado("que (.*) se cuentra en Swaglabs App")
     public void actorWantsBuyShirts(String actor) {
         theActorCalled(actor).attemptsTo(
                 Login.atSwagLabs()
         );
     }
 
-    @When("El coloca en el carrito la (.*)")
-    public void actorPutShirtCart(String nameProduct) {
-        theActorInTheSpotlight().attemptsTo(
-              AddProduct.toTheCart(nameProduct)
-        );
+    @Cuando("El coloca en el carrito la (.*)")
+    public void actorPutShirtCart() {
     }
 
-    @And("El hace el checkout de su compra")
+    @Y("El hace el checkout de su compra")
     public void checkoutPurchase() {
-        theActorInTheSpotlight().attemptsTo(checkout());
     }
 
-    @Then("El deberia de ver que su compra es exitosa")
+    @Entonces("El deberia de ver que su compra es exitosa")
     public void actorShouldSeePurchase() {
-        theActorInTheSpotlight().should(seeThat(Purchase.isSuccesful()).orComplainWith(PurchaseIsNotCompleteException.class, PURCHASE_FAILED_MESSAGE_EXCEPTION));
     }
 }
