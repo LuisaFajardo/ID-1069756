28a515426265dc471d47f0e9036effbb826914df
jfernandezt1 <jfernandez@todo1.net>
2021-02-09 15:07:19 -0500

    Ejemplo de ejercicios con Cucumber y diff escenarios

--- src/main/java/co/com/devco/userinterface/InicioPage.java
+++
@@ -1,9 +0,0 @@
-package co.com.devco.userinterface;
-
-import io.appium.java_client.MobileBy;
-import net.serenitybdd.screenplay.targets.Target;
-
-public class InicioPage {
-    public static final Target BTN_LUPA = Target.the("Boton para busqueda").located(MobileBy.AccessibilityId("Search…"));
-    public static final Target TXT_BUSQUEDA_PRODUCTOS = Target.the("Campo de texto para busqueda").located(MobileBy.id("com.solodroid.solomerce:id/search_src_text"));
-}
